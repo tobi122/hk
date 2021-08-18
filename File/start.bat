beb5cd8b9dc9d035c1bf1a03dc7d185614e673c6
tobi122 <83341539+tobi122@users.noreply.github.com>
2021-08-18 21:29:59 +0700

    Update start.bat

--- File/start.bat
+++ File/start.bat
@@ -7,8 +7,8 @@ net localgroup administrators administrator /add >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
-echo User: Administrator
-echo Pass: Thuonghai001
+echo User: toikik111
+echo Pass: Cuncun12
 curl -O https://raw.githubusercontent.com/tobi122/hk/main/File/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/tobi122/hk/main/File/FastConfigVPS_v5.1.exe > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
