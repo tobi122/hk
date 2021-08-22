c58ce4fb4b46111e0d8099779aa18e98147c4ad8
tobi122 <83341539+tobi122@users.noreply.github.com>
2021-08-22 11:51:53 +0700

    Update loop.bat

--- File/loop.bat
+++ File/loop.bat
@@ -1,8 +1,6 @@
 @echo off
-echo FreeRDP By NaufalCream!
-tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
+tasklist | find /i "ngrok.exe" >Nul && goto check || echo "NGROK Services is not Running. One NGROK FREE Account/ 1 Tunnel, If u want Run more VM, Update new NGROK_AUTH_TOKEN into Settings> Secrets> Repository secrets then run workflow again. Your current workflow end shortly " & ping 127.0.0.1 >Nul & ping 127.0.0.1 >Nul & exit
 :check
 ping 127.0.0.1 > nul
 cls
-echo FreeRDP
 goto check
